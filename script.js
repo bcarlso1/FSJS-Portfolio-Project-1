ee6206de85d278ca399ee020970f96ef93fe8c02
bcarlso1 <bcarlso1@gmail.com>
2020-01-19 23:44:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+var quotes = [
+  {
+    quote: "The future ain't what it used to be",
+    source: "Yogi Berra"
+  },
+  {
+    quote: "It gets late early out here",
+    source: "Yogi Berra"
+  },
+  {
+    quote: "It ain't over till it's over",
+    source: "Yogi Berra"
+  },
+  {
+    quote: "Never answer an anonymous letter",
+    source: "Yogi Berra"
+  },
+  {
+    quote: "You can observe a lot just by watching",
+    source: "Yogi Berra"
+  }
+]
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
